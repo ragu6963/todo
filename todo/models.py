58b1ff66a6efdcddb1081d891612731a77f1e0e8
wooyoung <bhj499956@likelion.org>
2019-05-20 18:38:54 +0900

    0520

--- todo/models.py
+++ todo/models.py
@@ -8,10 +8,13 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True) # 수정날짜 
     end_at = models.DateTimeField(null=True) # 마감날짜
     priority = models.IntegerField(default=1) # 우선순위
-    confirm = models.BooleanField(default=False)  
+    confirm = models.BooleanField(default=False) # 메모 확인 / 비확인
 
-    def d_day(self): 
-        if self.end_at == None:
+    class Meta: 
+        ordering = ['-updated_at'] #기본 정렬 순서 : 수정날짜 내림차순
+
+    def d_day(self): # 현재 날짜와 마감날짜 간의 차이 return method
+        if self.end_at == None: # 마감날짜 없을 시
             return "기한없음"
         else:
             time = self.end_at+timedelta(days=1) - datetime.now()
@@ -22,10 +25,6 @@ class Post(models.Model):
             elif time.days > 0:  
                 return "d-"+str(time.days)
 
-    def priority_range(self):
+    def priority_range(self): # 우선순위 범위
         
         return range(0,self.priority)
-
-    class Meta:
-        ordering = ['-updated_at'] 
-    
--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from .models import Post
 from datetime import datetime, timedelta
 
-
 def main(request):
     posts = Post.objects.all()
     context = {
@@ -13,38 +12,31 @@ def main(request):
 def index(request):
     if request.is_ajax():
         sort_filter = request.GET['sort_filter']
-        if sort_filter == "all":
+        if sort_filter == "all": # 모든 항목
             posts = Post.objects.all()
-        elif sort_filter =="confirm":
+        elif sort_filter =="confirm": # 완료된 항목
             posts = Post.objects.filter(confirm=True)
-        elif sort_filter =="un_confirm":
+        elif sort_filter =="un_confirm": # 미완료 항목
             posts = Post.objects.filter(confirm=False)
-        elif sort_filter =="end_post":
+        elif sort_filter =="end_post": # 마감날짜 지난 항목들 filter
             post = Post.objects.all()
             posts = [] 
             for p in post:
-                if p.end_at != None:
+                if p.end_at != None: # 마감날짜 성정 학목들 중에서
                     time = p.end_at+timedelta(days=1) - datetime.now()
-                    if time.days < 0:
-                        posts.append(p)   
-            context={'posts':posts} 
-            return render(request, 'index.html', context)
+                    if time.days < 0: # 마감날짜가 지난 항목들 list에 append
+                        posts.append(p)  
 
-        else:
+            context={'posts':posts} 
+            return render(request, 'index.html', context) 
+        else: # 우선순위 혹은 마감날짜, 내림차순 오름차순
             posts = Post.objects.order_by(sort_filter) 
 
         context = {
             'posts': posts,
         }
-        return render(request, 'index.html', context) 
-
-    else:  
-        posts = Post.objects.all()
-        context = { 
-            'posts': posts, 
-        }
-        return render(request, 'index.html', context) 
-
+        return render(request, 'index.html', context)  
+        
 def read(request):
     post_id = request.GET['post_id']
     post = Post.objects.get(id=post_id)
@@ -55,15 +47,14 @@ def read(request):
 
 def create(request):
     if request.method == "POST": 
-        now = datetime.now()
         post = Post()
         post.title = request.POST['title']
         post.content = request.POST['content']
         post.priority = request.POST['priority'] 
         try:
-            post.end_at = request.POST['end_at']
+            post.end_at = request.POST['end_at'] # 마감날짜 설정시
         except:
-            post.end_at = None
+            post.end_at = None # 마감날짜 미설정시
         post.save()
         return redirect('main')   
     else: 
@@ -87,8 +78,8 @@ def update(request):
         return redirect('main')
     else:
         if request.is_ajax(): 
-            post_id = request.GET['post_id']
-            post = Post.objects.get(id = post_id)
+            post_id = request.GET['post_id'] 
+            post = Post.objects.get(id = post_id) # 읽고있는 post table 불러오기
             context={
                 "post":post,
             }
@@ -96,17 +87,17 @@ def update(request):
 
 def delete(request): 
     if request.method == "POST":
-        post_id = request.POST['post_id']
+        post_id = request.POST['post_id'] # 읽고있는 post table 불러오기
         post = Post.objects.get(id = post_id) 
-        post.delete() 
+        post.delete() # table delete
         return redirect('main')
 
 def confirm(request):
     if request.method == "POST":
         post_id = request.POST['post_id']
         post = Post.objects.get(id = post_id) 
-        confirm_stat = post.confirm
-        post.confirm = not(confirm_stat)
+        confirm_stat = post.confirm # 현재 post confirm 상태 저장
+        post.confirm = not(confirm_stat) # 현재 post confirm 반대 값 저장
         post.save()  
         return redirect('main')
 
